d9efae796e95d23e6ec10b041a6bac38c82da962
emrecelik07 <emrcel07@gmail.com>
2025-07-23 12:02:14 +0300

    initial commit

---
+++ src/main/java/com/emrecelik/mini_instagram_backend/MiniInstagramBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emrecelik.mini_instagram_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniInstagramBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniInstagramBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emrecelik/mini_instagram_backend/MiniInstagramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrecelik.mini_instagram_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniInstagramBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
